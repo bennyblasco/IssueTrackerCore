ef5996b1d63e29781b01761f9eb621e9e40343de
Bennyblasco <bennyblasco@gmail.com>
2020-07-25 21:32:29 +0100

    Connection String

--- IssueTrackerCore/Data/ApplicationDbContext.cs
+++ IssueTrackerCore/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using IssueTrackerCore.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace IssueTrackerCore.Data
             : base(options)
         {
         }
+
+        public DbSet<Issue> Issue { get; set; }
     }
 }
